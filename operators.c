1e2c11112e22d0d3c8008c8a3831db4c84599448
Durgaduppula <149655886+Durgaduppula@users.noreply.github.com>
2023-11-03 11:47:16 +0530

    first commit

---
+++ operators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the value of a & b");
+    printf("%d /n",a+b);
+    printf("%d \n",a-b);
+    printf("%d \n" ,a*b);
+    printf("%d \n",a/b);
+    return 0;
+}
+     
